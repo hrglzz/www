f5303316d77abc90e38c4df648e7e240c68240e6
phiewter <19486709+phiewter@users.noreply.github.com>
2024-10-15 10:02:24 +0200

    ADD: <AlertDialog> component

--- app/playground/accordion/page.tsx
+++ app/playground/accordion/page.tsx
@@ -7,15 +7,13 @@ import {
 
 export default function AccordionPage() {
   return (
-    <div>
-      <Accordion type="single" collapsible>
-        <AccordionItem value="item-1">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
-          <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-    </div>
+    <Accordion type="single" collapsible>
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
   )
 }
